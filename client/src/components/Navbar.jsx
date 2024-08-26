732af91be02d1e1ffd2d5b15b60e0f5040b8cbc4
xframee <54414538+xframee@users.noreply.github.com>
2024-08-26 11:22:26 +0200

    add navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,22 @@
-import { AppBar, ToolBar } from '@mui/material';
+import { AppBar, Toolbar, IconButton, Typography, Stack, Button } from '@mui/material';
+import siteLogo from '../images/csraterlogo.png';
 
 export const NavBar = () => {
     return (
-        <div>
-            hej
-        </div>
+        <AppBar position='static' color='primary'>
+            <Toolbar>
+                <IconButton size='large' edge='start' aria-label='logo'>
+                    <img src={siteLogo} alt='site logo' style={{ width: '50px', height: 'auto' }} />
+                </IconButton>
+                <Typography variant='h6' sx={{ flexGrow: 1 }} textAlign="left">
+                    CS SKIN RATER
+                </Typography>
+                <Stack direction='row' spacing={2}>
+                    <Button color='inherit'>Item 1</Button>
+                    <Button color='inherit'>Item 2</Button>
+                    <Button color='inherit'>Item 3</Button>
+                </Stack>
+            </Toolbar>
+        </AppBar>
     );
 };
