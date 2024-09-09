5441b5dee1639c55e655c8caec5de0bbda7ac302
xframee <alexander@kmax.dk>
2024-09-09 14:18:06 +0200

    update navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,21 @@
 import { AppBar, Toolbar, IconButton, Typography, Stack, Button } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
 import siteLogo from '../images/csraterlogo.png';
 
 export const NavBar = () => {
     return (
         <AppBar position='static' color='primary'>
             <Toolbar>
-                <IconButton size='large' edge='start' aria-label='logo'>
+                <IconButton size='large' edge='start' aria-label='logo' component={RouterLink} to="/">
                     <img src={siteLogo} alt='site logo' style={{ width: '50px', height: 'auto' }} />
                 </IconButton>
                 <Typography variant='h6' sx={{ flexGrow: 1 }} textAlign="left">
                     CS SKIN RATER
                 </Typography>
                 <Stack direction='row' spacing={2}>
-                    <Button color='inherit'>Item 1</Button>
-                    <Button color='inherit'>Item 2</Button>
-                    <Button color='inherit'>Item 3</Button>
+                    <Button color='inherit' component={RouterLink} to="/path1">Item 1</Button>
+                    <Button color='inherit' component={RouterLink} to="/path2">Item 2</Button>
+                    <Button color='inherit' component={RouterLink} to="/path3">Item 3</Button>
                 </Stack>
             </Toolbar>
         </AppBar>
