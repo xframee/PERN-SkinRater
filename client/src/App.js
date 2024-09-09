b00a2207dbddf15ab02bbf757bcb88579b0f2d5c
xframee <alexander@kmax.dk>
2024-09-09 14:17:52 +0200

    updated app to use routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
-import {NavBar} from './components/Navbar';
+import { NavBar } from './components/Navbar';
+import { Route, Routes } from "react-router-dom";
+import { Home } from './pages/Home';
 
 function App() {
   return (
     <div className="App">
+
       <NavBar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
     </div>
   );
 }
