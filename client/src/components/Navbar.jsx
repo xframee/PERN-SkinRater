b95c19173ff4e5cf0b47c8b8266a78cc42b3e109
xframee <alexander@kmax.dk>
2024-09-17 13:17:49 +0200

    udpate navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,15 +7,15 @@ export const NavBar = () => {
         <AppBar position='static' sx={{ bgcolor: 'primary.dark' }}>
             <Toolbar>
                 <IconButton size='large' edge='start' aria-label='logo' component={RouterLink} to="/">
-                    <img src={siteLogo} alt='site logo' style={{ width: '50px', height: 'auto' }} />
+                    <img src={siteLogo} alt='site logo' style={{ width: '75px', height: 'auto' }} />
                 </IconButton>
                 <Typography variant='h6' sx={{ flexGrow: 1 }} textAlign="left">
                     CS SKIN RATER
                 </Typography>
                 <Stack direction='row' spacing={2}>
-                    <Button color='inherit' component={RouterLink} to="/path1">Item 1</Button>
-                    <Button color='inherit' component={RouterLink} to="/path2">Item 2</Button>
-                    <Button color='inherit' component={RouterLink} to="/path3">Item 3</Button>
+                    <Button color='inherit' component={RouterLink} to="/path1">Udforsk</Button>
+                    <Button color='inherit' component={RouterLink} to="/path2">Rate</Button>
+                    <Button color='inherit' component={RouterLink} to="/path3">Fisk</Button>
                 </Stack>
             </Toolbar>
         </AppBar>
