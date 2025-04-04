94225a0e863058ee2d4ddce28945b1c9767cb774
xframee <alexander@kmax.dk>
2025-04-04 13:18:33 +0200

    Update registerServive.js
    moved hashing from the frontened to the backend

--- client/src/utilities/registerServive.js
+++ client/src/utilities/registerServive.js
@@ -1,18 +1,12 @@
-import bcrypt from "bcryptjs";
-
 export const registerUser = async (username, password) => {
     try {
-        // Hash the password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
         // Send a POST request to the server
         const response = await fetch("http://localhost:5000/users", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ username, password: hashedPassword }),
+            body: JSON.stringify({ username, password }),
         });
 
         if (!response.ok) {
