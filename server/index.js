6451fccbb4af20498ed97218d32489d2a678ea2d
xframee <alexander@kmax.dk>
2025-04-22 12:20:47 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -180,7 +180,8 @@ app.post("/rate-skin", authenticateToken, async (req, res) => {
 });
 
 
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
