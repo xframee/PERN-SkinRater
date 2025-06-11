15f9ec220d005aac25929af53e58b95908a57138
xframee <alexander@kmax.dk>
2025-06-11 13:26:29 +0200

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,9 @@
 import { AppBar, Toolbar, IconButton, Typography, Stack, Button } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
 import siteLogo from '../images/csraterlogo.png';
-import { useState, useEffect } from 'react';
+import UserMenu from "./UserMenu";
 
 export const NavBar = () => {
-
-    const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("token"));
-
-    // keep login state in sync if e.g. another tab logs in/out
-    useEffect(() => {
-        const onStorage = () => setIsLoggedIn(!!localStorage.getItem("token"));
-        window.addEventListener("storage", onStorage);
-        return () => window.removeEventListener("storage", onStorage);
-    }, []);
 
     return (
         <AppBar position='static' sx={{ bgcolor: 'primary.dark' }}>
@@ -21,21 +12,12 @@ export const NavBar = () => {
                     <img src={siteLogo} alt='site logo' style={{ width: '75px', height: 'auto' }} />
                 </IconButton>
                 <Typography variant='h6' sx={{ flexGrow: 1 }} textAlign="left">
-                    CS SKIN RATER
+                    CSRDB
                 </Typography>
                 <Stack direction='row' spacing={2}>
                     <Button color='inherit' component={RouterLink} to="/path1">Explore</Button>
                     <Button color='inherit' component={RouterLink} to="/path2">Rate</Button>
-
-                    {isLoggedIn ? (
-                        <Button color="inherit" component={RouterLink} to="/logout">
-                            Logout
-                        </Button>
-                    ) : (
-                        <Button color="inherit" component={RouterLink} to="/login">
-                            Login
-                        </Button>
-                    )}
+                    <Button color='inherit' component={RouterLink} to="/login">Login</Button>
 
                 </Stack>
             </Toolbar>
