a6b43ac35ec407d109c39f6433803e44053e0c23
xframee <alexander@kmax.dk>
2025-06-18 11:56:37 +0200

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import SkinListCards from "../components/SkinListCards";
 
 export const Home = () => {
     return (
         <div>
             <h1>Home</h1>
+            <SkinListCards count={10} />
         </div>
     );
 }
