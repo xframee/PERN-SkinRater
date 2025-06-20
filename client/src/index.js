0ad3ab75537660f51717fc1d7f13967860139c55
xframee <alexander@kmax.dk>
2025-06-20 12:00:17 +0200

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,13 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom"
 import { UserProvider } from "./context/UserContext";
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './utilities/theme';
+import CssBaseline from '@mui/material/CssBaseline';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
