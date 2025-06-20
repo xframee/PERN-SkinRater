8a38b111d0fb0986d8c34831b5bdef0b010b2ac6
xframee <alexander@kmax.dk>
2025-06-20 11:47:12 +0200

    Update theme.jsx

--- client/src/utilities/theme.jsx
+++ client/src/utilities/theme.jsx
@@ -1,18 +1,25 @@
+// theme.js
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
   palette: {
+    mode: 'dark',
     primary: {
       light: '#757ce8',
-      main: '#3f50b5',
-      dark: '#252525',
+      main:  '#3f50b5',
+      dark:  '#252525',
       contrastText: '#fff',
     },
     secondary: {
       light: '#ff7961',
-      main: '#f44336',
-      dark: '#ba000d',
+      main:  '#f44336',
+      dark:  '#ba000d',
       contrastText: '#000',
+    },
+
+    background: {
+      default: '#121212',
+      paper:   '#1d1d1d',
     },
   },
 });
