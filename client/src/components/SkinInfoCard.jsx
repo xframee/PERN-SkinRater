345ebc829e1a04a07d9ca14eec1a4d948aa19c80
xframee <alexander@kmax.dk>
2025-06-25 23:08:13 +0200

    Update SkinInfoCard.jsx
    added text that tells the skin rarity

--- client/src/components/SkinInfoCard.jsx
+++ client/src/components/SkinInfoCard.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
+import Box from '@mui/material/Box';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -9,7 +10,7 @@ import '../App.css';
 
 //make a color object that maps to the skin rarity and then we style each card based on the rarity
 const rarityColors = {
-    "Consumer" : "#AFAFAF",
+    "Consumer": "#AFAFAF",
     "Industrial": "#6496E1",
     "Mil-spec": "#4B69CD",
     "Restricted": "#8847FF",
@@ -23,6 +24,11 @@ export default function SkinInfoCard({ skin, refetchSkins }) {
     return (
         <div className="skin-info-cards">
             <Card sx={{ width: 320, borderRadius: 2, bgcolor: rarityColors[skin.rarity] || '#FFFFFF' }}>
+                <Box sx={{ p: 1.5, bgcolor: 'rgba(0,0,0,0.5)' }}>
+                    <Typography variant="h6" color="#ffffff" sx={{ textTransform: 'capitalize' }}>
+                        {skin.rarity}
+                    </Typography>
+                </Box>
                 <CardMedia
                     sx={{ height: 240 }}
                     image={skin.skin_image}
@@ -34,7 +40,7 @@ export default function SkinInfoCard({ skin, refetchSkins }) {
                 </CardContent>
                 <CardActions sx={{ bgcolor: 'background.paper', borderTop: '2px solid #e0e0e0', justifyContent: 'space-between' }}>
                     <Typography >Average rating: {skin.average_rating ?? "N/A"}</Typography>
-                    <RateMenu skinId={ skin.skin_id } onRated={refetchSkins} />
+                    <RateMenu skinId={skin.skin_id} onRated={refetchSkins} />
                 </CardActions>
             </Card>
         </div>
