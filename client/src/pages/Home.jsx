9d91395f1b43e05ed003d7d5d0c45dc0f12a0206
xframee <alexander@kmax.dk>
2025-06-28 13:37:51 +0200

    Update Home.jsx
    added hero caroussel

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import SkinListCards from "../components/SkinListCards";
+import { HeroCarousel } from "../components/HeroCaroussel";
 
 export const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <HeroCarousel />
             <SkinListCards count={8} />
         </div>
     );
