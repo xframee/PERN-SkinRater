cc826f3bece59c29e26ac19891df1e0a7717de9b
xframee <alexander@kmax.dk>
2025-06-28 13:38:07 +0200

    Update HeroCaroussel.jsx
    added a hero caroussel (needs more styling)

---
+++ client/src/components/HeroCaroussel.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Carousel from 'react-material-ui-carousel'
+import { Paper } from '@mui/material'
+import akAquamarine from '../images/ak_aqumarine.jpg';
+import akWildLotus from '../images/ak_wild_lotus.jpg';
+
+export const HeroCarousel = () => {
+    var items = [
+        {
+            name: "Fist element",
+            description: "Random description for the first element",
+            image: akAquamarine
+        },
+        {
+            name: "Second element",
+            description: "Random description for the second element",
+            image: akWildLotus
+        }
+    ]
+
+    return (
+        <Carousel
+            indicatorContainerProps={{
+                style: {
+                    marginTop: '20px',
+                    transitionDuration: '0.5s'
+                }
+            }}
+
+            indicatorIconButtonProps={{
+                style: { color: '#222222' }
+            }}
+            activeIndicatorIconButtonProps={{
+                style: { color: '#EEEEEE' }
+            }}
+        >
+            {
+                items.map((item, i) => <Item key={i} item={item} />)
+            }
+        </Carousel>
+    )
+
+    function Item(props) {
+        return (
+            <Paper>
+                <img src={props.item.image} alt={props.item.name} style={{ width: "100%", maxHeight: "600px"}} />
+            </Paper>
+        )
+    }
+}
