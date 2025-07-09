2f98d70716ba935ab5a43eb89040beeb5135a59d
xframee <alexander@kmax.dk>
2025-07-09 09:38:04 +0200

    Update App.js
    added path to all skins

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { NavBar } from './components/Navbar';
 import { Login } from './pages/Login';
 import { Home } from './pages/Home';
 import { Register } from './pages/Register';
+import {Skins} from './pages/Skins'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/skins" element={<Skins />} />
         </Routes>
       </div>
     </ThemeProvider>
