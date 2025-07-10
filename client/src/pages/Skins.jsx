62a88e9263e0a3154a726b221209ef51c327b9ab
Alexander Christensen <alexander@kmax.dk>
2025-07-10 11:39:03 +0200

    Update Skins.jsx

--- client/src/pages/Skins.jsx
+++ client/src/pages/Skins.jsx
@@ -12,7 +12,7 @@ export const Skins = () => {
             #Add button to change the filter and sorting
             <h1>Here you can see all skins currently in Counter Strike 2</h1>
             <Button variant="contained" onClick={() => setFilter("newest")}>Newest</Button>
-            <SkinListCards count={12} title={"All Skins"} filter={"newest"} />
+            <SkinListCards count={12} title={"All Skins"} filter={filter} />
         </>
     );
 };
