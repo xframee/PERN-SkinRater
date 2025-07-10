9ed9f4f66262aac68c9137e1d692478b8a585ae0
Alexander Christensen <alexander@kmax.dk>
2025-07-10 11:29:03 +0200

    fix filter logic

--- client/src/pages/Skins.jsx
+++ client/src/pages/Skins.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SkinListCards from "../components/SkinListCards";
 import { useState } from "react";
+import Button from '@mui/material/Button';
 
 export const Skins = () => {
 
@@ -10,6 +11,7 @@ export const Skins = () => {
         <>
             #Add button to change the filter and sorting
             <h1>Here you can see all skins currently in Counter Strike 2</h1>
+            <Button variant="contained" onClick={() => setFilter("newest")}>Newest</Button>
             <SkinListCards count={12} title={"All Skins"} filter={"newest"} />
         </>
     );
