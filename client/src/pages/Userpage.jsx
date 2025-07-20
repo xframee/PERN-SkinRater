f7cacdf4db3285f05a132268eaa810479b7452ee
Alexander Christensen <alexander@kmax.dk>
2025-07-20 22:57:36 +0200

    add userpage

---
+++ client/src/pages/Userpage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Userpage = () => {
+    return (
+        <div>
+            <h1>Userpage</h1>
+            <p>Here you can see your skins and your profile.</p>
+            <p>Currently this page is not implemented.</p>
+        </div>
+    );
+}
